77d0488fdc0c3a96e759aa50818f776680258ca6
Joshua Onabanjo <onabanjotobi@gmail.com>
2022-07-04 17:44:14 +0100

    Created indicators container

--- app/src/main/java/com/marannix/onboardingcarousel/OnboardingActivity.kt
+++ app/src/main/java/com/marannix/onboardingcarousel/OnboardingActivity.kt
@@ -2,18 +2,29 @@ package com.marannix.onboardingcarousel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.marannix.onboardingcarousel.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityOnboardingBinding
     private lateinit var adapter: OnboardingItemsAdapter
+    private lateinit var indicatorsContainerLayout: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOnboardingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        indicatorsContainerLayout = binding.indicatorsContainer
         setOnboardingItems()
+        setupIndicators()
+        setCurrentIndicator(0)
+        setViewPagerListener()
     }
 
     private fun setOnboardingItems() = with(binding) {
@@ -38,5 +49,64 @@ class OnboardingActivity : AppCompatActivity() {
             )
         )
         onboardingViewPager.adapter = adapter
+    }
+
+    private fun setViewPagerListener() = with(binding) {
+        onboardingViewPager.adapter = adapter
+        onboardingViewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicator(position)
+            }
+        })
+
+        (onboardingViewPager.getChildAt(0) as RecyclerView).overScrollMode =
+            RecyclerView.OVER_SCROLL_NEVER
+    }
+
+    //We will now iterate through the size of the adapter and create multiple inactive background states
+    private fun setupIndicators() = with(binding) {
+        indicatorsContainerLayout = indicatorsContainer
+        val indicators = arrayOfNulls<ImageView>(adapter.itemCount)
+        val layoutParams: LinearLayout.LayoutParams =
+            LinearLayout.LayoutParams(WRAP_CONTENT, WRAP_CONTENT)
+        layoutParams.setMargins(8, 8, 8, 0)
+
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(applicationContext)
+            indicators[i]?.let {
+                it.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+                it.layoutParams = layoutParams
+                indicatorsContainerLayout.addView(it)
+            }
+        }
+    }
+
+    private fun setCurrentIndicator(position: Int) = with(binding) {
+        val childCount = indicatorsContainerLayout.childCount
+        for (i in 0 until childCount) {
+            val imageView = indicatorsContainerLayout.getChildAt(i) as ImageView
+            if (i == position) {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_active_background
+                    )
+                )
+            } else {
+                imageView.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        applicationContext,
+                        R.drawable.indicator_inactive_background
+                    )
+                )
+            }
+        }
     }
 }
